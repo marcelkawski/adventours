6a8eef337a61c0a685c3241f4175891e25c4f8e9
marcelkawski <marcel.kawski1@gmail.com>
2022-07-05 14:27:18 +0200

    Create tour schema and model

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    // local db
+    // .connect(process.env.DATABASE_LOCAL, {
+    // hosted db
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log('Database connection successful!');
+    });
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name.'],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 0.0,
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price.'],
+    },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
