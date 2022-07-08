edbc4a961349e658e8aa03ab6601daa0604e66f0
marcelkawski <marcel.kawski1@gmail.com>
2022-07-08 09:04:57 +0200

    Add tours fields limiting

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -26,6 +26,14 @@ exports.getAllTours = async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        // fields limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
         // execute query
         const tours = await query;
 
