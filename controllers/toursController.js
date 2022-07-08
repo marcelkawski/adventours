7e4a76d6adaf82d2b2d06f76222cb83b0a92fc35
marcelkawski <marcel.kawski1@gmail.com>
2022-07-08 11:27:17 +0200

    Add checking error for pagination

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -37,9 +37,16 @@ exports.getAllTours = async (req, res) => {
         }
 
         // pagination
+        // checking if the user requests page for which: page * limit > number of documents
         const page = req.query.page * 1 || 1;
         const limit = req.query.limit * 1 || 100;
         const skip = (page - 1) * limit;
+
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page does not exist.');
+        }
+
         query = query.skip(skip).limit(limit);
 
         // execute query
