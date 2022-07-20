43905792215ce71335fda21a3207aa026b6d91e7
marcelkawski <marcel.kawski1@gmail.com>
2022-07-20 10:04:04 +0200

    Add error handlers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const toursRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -15,16 +17,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware 👋');
-    next();
-});
-
-app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
+
+app.all('*', (req, res, next) => {
+    next(
+        new AppError(`Could not find ${req.originalUrl} on this server :(`),
+        404
+    );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
