28d6dd39364542ea2d4628f64e4017531ddd034e
marcelkawski <marcel.kawski1@gmail.com>
2022-08-29 09:30:07 +0200

    Populate guides in tours model

--- models/toursModel.js
+++ models/toursModel.js
@@ -165,6 +165,15 @@ tourSchema.pre(/^find/, function (next) {
     next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+    // this.populate('guides'); // getting all fields; pupulate - to make tours output look like guides were embedded (but they are referenced actually).
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt',
+    });
+    next();
+});
+
 tourSchema.post(/^find/, function (documents, next) {
     console.log(`Query took: ${Date.now() - this.start} milliseconds.`);
     next();
