75b6cf1ad8f14cb54899621add1def2cd1018f6b
marcelkawski <marcel.kawski1@gmail.com>
2022-09-08 14:18:57 +0200

    Update import-dev-data.js script for importing users and reviews

--- data/import-dev-data.js
+++ data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const Tour = require('./../models/toursModel');
+const User = require('./../models/usersModel');
+const Review = require('./../models/reviewsModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -24,11 +26,18 @@ mongoose
 
 // read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/reviews.json`, 'utf8')
+);
 
 // import data into the database
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false }); // We need to turn off validation because we create users without passwordConfirm property (it is not in users.json file). We also had to comment lines with password encryption in users model.
+        await Review.create(reviews);
+
         console.log('Data successfully imported into the database!');
     } catch (err) {
         console.log(err);
@@ -40,6 +49,9 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
+
         console.log('Data successfully deleted from the database.');
     } catch (err) {
         console.log(err);
