a2f01f39cf36d149cc86926006b370057370f1af
marcelkawski <marcel.kawski1@gmail.com>
2022-10-08 20:08:57 +0200

    Add login template and connect it

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,7 @@
 const Tour = require('./../models/toursModel');
-const Review = require('./../models/reviewsModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
     const tours = await Tour.find();
 
     res.status(200).render('overview', {
@@ -11,7 +10,7 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
         path: 'reviews',
         fields: 'review rating author',
@@ -22,3 +21,9 @@ exports.getTour = catchAsync(async (req, res) => {
         tour,
     });
 });
+
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login', {
+        title: 'Log into your account',
+    });
+};
